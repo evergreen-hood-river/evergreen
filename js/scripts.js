0a6eb406e57d0a3da15a57da9d9f8c7b95efb95f
Josh Bertsche <jjjbertsche@gmail.com>
2016-09-03 19:58:30 -0700

    compressed images

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,11 @@
+
+// prevent background dragging on mobile
 var fixed = document.body;
 fixed.addEventListener('touchmove', function (e) {
   e.preventDefault();
 }, false);
 
+// set up mailing list button
 function showMailingPopUp() {
   require(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us10.list-manage.com","uuid":"d4ab9c169567794693d5a21ca","lid":"c9006bbf5c"}) })
     document.cookie = "MCEvilPopupClosed=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
