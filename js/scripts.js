4d4fae5fdad7e960fa9536183080c0cc5a76f4f4
Josh Bertsche <jjjbertsche@gmail.com>
2016-10-18 20:35:30 -0700

    fixed cropping, broke other things

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,19 @@
 // prevent background dragging on mobile
 var fixed = document.body;
-fixed.addEventListener('touchmove', function (e) {
+fixed.addEventListener('touchmove', function(e) {
   e.preventDefault();
 }, false);
 
 // set up mailing list button
 function showMailingPopUp() {
-  require(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us14.list-manage.com","uuid":"9ced1de5481219e14964f2cdd","lid":"6cf0551d72"}) })
-    document.cookie = "MCEvilPopupClosed=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
-};
+  require(["mojo/signup-forms/Loader"], function(L) {
+    L.start({"baseUrl": "mc.us14.list-manage.com", "uuid": "9ced1de5481219e14964f2cdd", "lid": "6cf0551d72"});
+  });
+  document.cookie = "MCEvilPopupClosed=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+}
 document.getElementById("open-popup").onclick = function(e) {
   e.preventDefault();
-  showMailingPopUp()
+  showMailingPopUp();
 };
 
 // enter/leave buttons
